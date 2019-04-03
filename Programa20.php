b15aa64bb91debd3a8d3da692764ae864452b91b
kowDown <48135661+kowDown@users.noreply.github.com>
2019-04-02 23:59:04 -0300

    Create Programa20.php

---
+++ Programa20.php
@@ -0,0 +1,39 @@
+<?php
+
+for ($a = 0; $a < 8; $a++) { 
+
+    print "* * * * * * * * \n";
+
+}
+
+print "\n";
+
+//Inspiração: https://github.com/marcuscostagraciano/estudo_dirigido2/blob/master/exercicio20.php
+
+for ($e = 0; $e < 8; $e++) {
+
+    for ($i = -1; $i < $e; $i++) {
+
+       print "* ";
+
+    }
+
+    print "\n";
+
+}
+
+print "\n";
+
+//Inspiração: https://github.com/marcuscostagraciano/estudo_dirigido2/blob/master/exercicio20.php
+
+for ($o = 0; $o < 8; $o++) {
+
+    for ($u = 8; $u > $o; $u--){
+
+        print "* ";
+
+    }
+
+    print "\n";
+
+}

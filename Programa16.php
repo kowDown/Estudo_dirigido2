f311d51092a48cdbf0e224fba72604262d51ff79
kowDown <48135661+kowDown@users.noreply.github.com>
2019-04-02 23:55:52 -0300

    Create Programa16.php

---
+++ Programa16.php
@@ -0,0 +1,21 @@
+<?php 
+
+print "Digite o primeiro número: ";
+        $num1 =(float) fgets(STDIN);
+
+print "Digite o primeiro número: ";
+        $num2 =(float) fgets(STDIN);
+
+print "Digite o primeiro número: ";
+        $num3 =(float) fgets(STDIN);
+
+print "Digite o primeiro número: ";
+        $num4 =(float) fgets(STDIN);
+
+print "Digite o primeiro número: ";
+        $num5 =(float) fgets(STDIN);
+
+    $soma = ($num1 + $num2 + $num3 + $num4 + $num5);
+       $media = ($soma / 5);
+
+       print "média: $media";

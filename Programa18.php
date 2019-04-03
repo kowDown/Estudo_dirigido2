35d4728696199c6ceb3b9de4455eeca306134e32
kowDown <48135661+kowDown@users.noreply.github.com>
2019-04-02 23:56:29 -0300

    Create Programa18.php

---
+++ Programa18.php
@@ -0,0 +1,22 @@
+<?php
+
+$numeropar=0;
+
+$numeroimpar=0;
+
+$contagemvezes=1;
+
+do {print "Digite um número: ";
+    
+    $num = (float) fgets(STDIN);
+    
+    if ($num%2==0)
+
+    {$numeropar++;}
+
+    if ($num%2!=0)
+    {$numeroimpar++;}  
+
+    $contagemvezes++;}  while($contagemvezes<=10);
+
+print "Quantidade de número(s) impar(es): $numeroimpar\nQuantidade de número(s) par(es): $numeropar";
